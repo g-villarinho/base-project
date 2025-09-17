90adfedf4f4f0cfb0d6ef2f3c1a4e1c4e5fe32a3
Villas <gabrielvillarinho01@gmail.com>
2025-09-17 00:41:17 -0300

    feat: add session repository

---
+++ internal/repository/session.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/g-villarinho/user-demo/internal/domain"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrSessionNotFound = errors.New("session record not found")
+)
+
+type SessionRepository interface {
+	Create(ctx context.Context, session *domain.Session) error
+  FindByID(ctx context.Context, ID uuid.UUID) (*domain.Session, error)
+}
+
+type sessionRepository struct {
+	db *gorm.DB
+}
+
+func NewSessionRepository(db *gorm.DB) SessionRepository {
+	return &sessionRepository{
+		db: db,
+	}
+}
+
+func (r *sessionRepository) Create(ctx context.Context, session *domain.Session) error {
+	if err := r.db.Create(&session).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *sessionRepository) FindByID(ctx context.Context, ID uuid.UUID) (*domain.Session, error) {
+	var session domain.Session
+
+	if err := r.db.First(&session, "id = ?", ID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, ErrSessionNotFound
+		}
+
+		return nil, err
+	}
+
+	return &session, nil
+}
