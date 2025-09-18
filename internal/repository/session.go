622ee2175d1ba089eed85abf8c3201aecbe0ae66
Villas <gabrielvillarinho01@gmail.com>
2025-09-17 22:17:13 -0300

    feat: add revoke session methods in repository

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/g-villarinho/user-demo/internal/domain"
 	"github.com/google/uuid"
@@ -16,6 +17,8 @@ var (
 type SessionRepository interface {
 	Create(ctx context.Context, session *domain.Session) error
   FindByID(ctx context.Context, ID uuid.UUID) (*domain.Session, error)
+	RevokeByUserID(ctx context.Context, userID uuid.UUID) error
+	Revoke(ctx context.Context, ID uuid.UUID) error
 }
 
 type sessionRepository struct {
@@ -29,7 +32,7 @@ func NewSessionRepository(db *gorm.DB) SessionRepository {
 }
 
 func (r *sessionRepository) Create(ctx context.Context, session *domain.Session) error {
-	if err := r.db.Create(&session).Error; err != nil {
+	if err := r.db.WithContext(ctx).Create(&session).Error; err != nil {
 		return err
 	}
 
@@ -39,7 +42,7 @@ func (r *sessionRepository) Create(ctx context.Context, session *domain.Session)
 func (r *sessionRepository) FindByID(ctx context.Context, ID uuid.UUID) (*domain.Session, error) {
 	var session domain.Session
 
-	if err := r.db.First(&session, "id = ?", ID).Error; err != nil {
+	if err := r.db.WithContext(ctx).First(&session, "id = ?", ID).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return nil, ErrSessionNotFound
 		}
@@ -49,3 +52,48 @@ func (r *sessionRepository) FindByID(ctx context.Context, ID uuid.UUID) (*domain
 
 	return &session, nil
 }
+
+func (r *sessionRepository) RevokeByUserID(ctx context.Context, userID uuid.UUID) error {
+	now := time.Now().UTC()
+
+	updates := map[string]any{
+		"revoked_at": now,
+		"updated_at": now,
+	}
+
+	err := r.db.WithContext(ctx).
+	  Model(&domain.Session{}).
+		Where("user_id = ? AND revoked_at IS NOT NULL", userID).
+		Updates(updates).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil	
+}
+
+func (r *sessionRepository) Revoke(ctx context.Context, ID uuid.UUID) error {
+	now := time.Now().UTC()
+
+	updates := map[string]any{
+		"revoked_at": now,
+		"updated_at": now,
+	}
+
+	result := r.db.WithContext(ctx).
+	  Model(&domain.Session{}).
+		Where("id = ? AND revoked_at IS NOT NULL", ID).
+		Updates(updates)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return ErrSessionNotFound
+	}
+
+	return nil	
+}
+
